95167d30117b828dc733bdd3b77984525f4eb140
houndhollis <houndhollis9797@gmail.com>
2025-06-03 21:05:45 +0900

    feat: 매직넘버 위치변경

--- src/SearchDialog.tsx
+++ src/SearchDialog.tsx
@@ -33,7 +33,7 @@ import { useScheduleContext } from "./ScheduleContext.tsx";
 import { Lecture } from "./types.ts";
 import { parseSchedule } from "./utils.ts";
 import axios, { AxiosResponse } from "axios";
-import { DAY_LABELS } from "./constants.ts";
+import { DAY_LABELS, TIME_SLOTS } from "./constants.ts";
 
 interface Props {
   searchInfo: {
@@ -52,33 +52,6 @@ interface SearchOption {
   majors: string[];
   credits?: number;
 }
-
-const TIME_SLOTS = [
-  { id: 1, label: "09:00~09:30" },
-  { id: 2, label: "09:30~10:00" },
-  { id: 3, label: "10:00~10:30" },
-  { id: 4, label: "10:30~11:00" },
-  { id: 5, label: "11:00~11:30" },
-  { id: 6, label: "11:30~12:00" },
-  { id: 7, label: "12:00~12:30" },
-  { id: 8, label: "12:30~13:00" },
-  { id: 9, label: "13:00~13:30" },
-  { id: 10, label: "13:30~14:00" },
-  { id: 11, label: "14:00~14:30" },
-  { id: 12, label: "14:30~15:00" },
-  { id: 13, label: "15:00~15:30" },
-  { id: 14, label: "15:30~16:00" },
-  { id: 15, label: "16:00~16:30" },
-  { id: 16, label: "16:30~17:00" },
-  { id: 17, label: "17:00~17:30" },
-  { id: 18, label: "17:30~18:00" },
-  { id: 19, label: "18:00~18:50" },
-  { id: 20, label: "18:55~19:45" },
-  { id: 21, label: "19:50~20:40" },
-  { id: 22, label: "20:45~21:35" },
-  { id: 23, label: "21:40~22:30" },
-  { id: 24, label: "22:35~23:25" },
-];
 
 const PAGE_SIZE = 100;
 
@@ -172,10 +145,15 @@ const SearchDialog = ({ searchInfo, onClose }: Props) => {
     [lectures, searchOptions]
   );
 
-  console.log(filteredLectures);
   const lastPage = Math.ceil(filteredLectures.length / PAGE_SIZE);
   const visibleLectures = filteredLectures.slice(0, page * PAGE_SIZE);
-  const allMajors = [...new Set(lectures.map((lecture) => lecture.major))];
+  const allMajors = useMemo(() => {
+    return [
+      ...new Set(
+        lectures.map((lecture) => lecture.major.replace(/<p>/gi, " "))
+      ),
+    ];
+  }, [lectures]);
 
   const changeSearchOption = (
     field: keyof SearchOption,
@@ -403,7 +381,7 @@ const SearchDialog = ({ searchInfo, onClose }: Props) => {
                     {allMajors.map((major) => (
                       <Box key={major}>
                         <Checkbox key={major} size="sm" value={major}>
-                          {major.replace(/<p>/gi, " ")}
+                          {major}
                         </Checkbox>
                       </Box>
                     ))}
