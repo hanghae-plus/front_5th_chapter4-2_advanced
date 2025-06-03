2c75071392e5b125faf0190fff7d524b72da155e
chloekim <chloe@perfitt.io>
2025-06-04 00:52:00 +0900

    refactor: fetchAllLectures 병렬로 실행되게 수정하기

--- src/SearchDialog.tsx
+++ src/SearchDialog.tsx
@@ -33,7 +33,7 @@ import { useEffect, useRef, useState } from "react";
 import { DAY_LABELS } from "./constants.ts";
 import { useScheduleContext } from "./ScheduleContext.tsx";
 import { Lecture } from "./types.ts";
-import { parseSchedule } from "./utils.ts";
+import { createCachedFetcher, parseSchedule } from "./utils.ts";
 
 interface Props {
   searchInfo: {
@@ -86,19 +86,17 @@ const fetchMajors = () => axios.get<Lecture[]>("/schedules-majors.json");
 const fetchLiberalArts = () =>
   axios.get<Lecture[]>("/schedules-liberal-arts.json");
 
-// TODO: 이 코드를 개선해서 API 호출을 최소화 해보세요 + Promise.all이 현재 잘못 사용되고 있습니다. 같이 개선해주세요.
-//Promise.all 안에서 await를 써서 병렬 처리가 안 되고 있음
-//fetchMajors, fetchLiberalArts를 중복 호출하고 있음
+const fetchMajorsCached = createCachedFetcher(fetchMajors);
+const fetchLiberalArtsCached = createCachedFetcher(fetchLiberalArts);
 
-const fetchAllLectures = async () =>
-  await Promise.all([
-    (console.log("API Call 1", performance.now()), await fetchMajors()),
-    (console.log("API Call 2", performance.now()), await fetchLiberalArts()),
-    (console.log("API Call 3", performance.now()), await fetchMajors()),
-    (console.log("API Call 4", performance.now()), await fetchLiberalArts()),
-    (console.log("API Call 5", performance.now()), await fetchMajors()),
-    (console.log("API Call 6", performance.now()), await fetchLiberalArts()),
+const fetchAllLectures = async () => {
+  const [majors, liberalArts] = await Promise.all([
+    (console.log("API Call 1", performance.now()), fetchMajorsCached()),
+    (console.log("API Call 2", performance.now()), fetchLiberalArtsCached()),
   ]);
+
+  return [majors, liberalArts];
+};
 
 // TODO: 이 컴포넌트에서 불필요한 연산이 발생하지 않도록 다양한 방식으로 시도해주세요.
 const SearchDialog = ({ searchInfo, onClose }: Props) => {
