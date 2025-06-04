9416b9fddedc201af1630027477a0de092f07c76
서주희 <maywngml@naver.com>
2025-06-04 23:06:04 +0900

    getFilteredLectures 함수를 컴포넌트 외부로 분리, filteredLectures 등 메모이제이션

--- src/SearchDialog.tsx
+++ src/SearchDialog.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import {
   Box,
   Button,
@@ -101,6 +101,40 @@ const fetchAllLectures = async () =>
     (console.log("API Call 6", performance.now()), fetchLiberalArts()),
   ]);
 
+const getFilteredLectures = (
+  searchOptions: SearchOption,
+  lectures: Lecture[],
+) => {
+  const { query = "", credits, grades, days, times, majors } = searchOptions;
+  return lectures
+    .filter(
+      (lecture) =>
+        lecture.title.toLowerCase().includes(query.toLowerCase()) ||
+        lecture.id.toLowerCase().includes(query.toLowerCase()),
+    )
+    .filter((lecture) => grades.length === 0 || grades.includes(lecture.grade))
+    .filter((lecture) => majors.length === 0 || majors.includes(lecture.major))
+    .filter(
+      (lecture) => !credits || lecture.credits.startsWith(String(credits)),
+    )
+    .filter((lecture) => {
+      if (days.length === 0) {
+        return true;
+      }
+      const schedules = lecture.schedule ? parseSchedule(lecture.schedule) : [];
+      return schedules.some((s) => days.includes(s.day));
+    })
+    .filter((lecture) => {
+      if (times.length === 0) {
+        return true;
+      }
+      const schedules = lecture.schedule ? parseSchedule(lecture.schedule) : [];
+      return schedules.some((s) =>
+        s.range.some((time) => times.includes(time)),
+      );
+    });
+};
+
 // TODO: 이 컴포넌트에서 불필요한 연산이 발생하지 않도록 다양한 방식으로 시도해주세요.
 const SearchDialog = ({ searchInfo, onClose }: Props) => {
   const { setSchedulesMap } = useScheduleContext();
@@ -117,49 +151,22 @@ const SearchDialog = ({ searchInfo, onClose }: Props) => {
     majors: [],
   });
 
-  const getFilteredLectures = () => {
-    const { query = "", credits, grades, days, times, majors } = searchOptions;
-    return lectures
-      .filter(
-        (lecture) =>
-          lecture.title.toLowerCase().includes(query.toLowerCase()) ||
-          lecture.id.toLowerCase().includes(query.toLowerCase()),
-      )
-      .filter(
-        (lecture) => grades.length === 0 || grades.includes(lecture.grade),
-      )
-      .filter(
-        (lecture) => majors.length === 0 || majors.includes(lecture.major),
-      )
-      .filter(
-        (lecture) => !credits || lecture.credits.startsWith(String(credits)),
-      )
-      .filter((lecture) => {
-        if (days.length === 0) {
-          return true;
-        }
-        const schedules = lecture.schedule
-          ? parseSchedule(lecture.schedule)
-          : [];
-        return schedules.some((s) => days.includes(s.day));
-      })
-      .filter((lecture) => {
-        if (times.length === 0) {
-          return true;
-        }
-        const schedules = lecture.schedule
-          ? parseSchedule(lecture.schedule)
-          : [];
-        return schedules.some((s) =>
-          s.range.some((time) => times.includes(time)),
-        );
-      });
-  };
-
-  const filteredLectures = getFilteredLectures();
-  const lastPage = Math.ceil(filteredLectures.length / PAGE_SIZE);
-  const visibleLectures = filteredLectures.slice(0, page * PAGE_SIZE);
-  const allMajors = [...new Set(lectures.map((lecture) => lecture.major))];
+  const filteredLectures = useMemo(
+    () => getFilteredLectures(searchOptions, lectures),
+    [searchOptions, lectures],
+  );
+  const lastPage = useMemo(
+    () => Math.ceil(filteredLectures.length / PAGE_SIZE),
+    [filteredLectures],
+  );
+  const visibleLectures = useMemo(
+    () => filteredLectures.slice(0, page * PAGE_SIZE),
+    [page, filteredLectures],
+  );
+  const allMajors = useMemo(
+    () => [...new Set(lectures.map((lecture) => lecture.major))],
+    [lectures],
+  );
 
   const changeSearchOption = (
     field: keyof SearchOption,
