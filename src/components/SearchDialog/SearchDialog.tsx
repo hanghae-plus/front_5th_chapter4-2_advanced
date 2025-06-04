a049e376b2baf53378e16e5ecd2740118b09174c
서주희 <maywngml@naver.com>
2025-06-05 00:54:12 +0900

    초기 강의 로드 로직 오류 수정

--- src/components/SearchDialog/SearchDialog.tsx
+++ src/components/SearchDialog/SearchDialog.tsx
@@ -199,7 +199,9 @@ const SearchDialog = ({ searchInfo, onClose }: Props) => {
       const end = performance.now();
       console.log("모든 API 호출 완료 ", end);
       console.log("API 호출에 걸린 시간(ms): ", end - start);
-      setLectures(results.flatMap((result) => result.data));
+
+      const validResults = results.filter((result) => result !== null);
+      setLectures(validResults.flatMap((result) => result.data));
     });
   }, []);
 
