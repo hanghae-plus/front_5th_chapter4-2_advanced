2a653598e8cb32c77e05fa5c6f05fb11b5545634
luwbe1 <luwbe1@naver.com>
2025-06-04 23:50:30 +0900

    Refactor: api 수정

--- src/api.ts
+++ src/api.ts
@@ -4,21 +4,21 @@ import { Lecture } from './types.ts';
 export const fetchMajors = () => axios.get<Lecture[]>('/schedules-majors.json');
 export const fetchLiberalArts = () => axios.get<Lecture[]>('/schedules-liberal-arts.json');
 
-let cache: Lecture[] | null = null;
-let fetching: Promise<Lecture[]> | null = null;
+let cache: Lecture[] = [];
+let isLoaded = false;
 
 export const fetchAllLectures = async (): Promise<Lecture[]> => {
-  // 캐시된 결과가 있다면 그대로 반환
-  if (cache) return cache;
-  // 이미 fetching 중인 경우, 해당 Promise를 반환
-  if (fetching) return fetching;
+  if (cache.length > 0) return cache;
 
-  // 캐시가 없고 fetching 중이지 않다면, 데이터를 가져오는 Promise를 생성
-  fetching = Promise.all([fetchMajors(), fetchLiberalArts()]).then(([majors, liberal]) => {
-    cache = [...majors.data, ...liberal.data]; // 결과 캐싱
-    return cache;
+  // majors 먼저 가져오기
+  const majors = await fetchMajors();
+  cache = [...majors.data];
+
+  // 백그라운드로 교양도 가져와서 병합 (await 안 함)
+  fetchLiberalArts().then((liberal) => {
+    cache = [...cache, ...liberal.data];
+    isLoaded = true;
   });
 
-  // 진행중인 fetching Promise를 반환
-  return fetching;
+  return cache;
 };
