557a49684bd2442ffc4aea81a9ff5f6c212f13f2
jaeyeonjung <jaeyeonjung3@gmail.com>
2025-06-04 17:35:15 +0900

    perf: 전공 목록을 조회 리렌더링 최적화

--- src/SearchDialog.tsx
+++ src/SearchDialog.tsx
@@ -34,7 +34,7 @@ import { Lecture } from "./types.ts";
 import { parseSchedule } from "./utils.ts";
 import axios, { AxiosResponse } from "axios";
 import { DAY_LABELS } from "./constants.ts";
-
+import SearchClass from "./searchClass.tsx";
 interface Props {
   searchInfo: {
     tableId: string;
@@ -323,34 +323,11 @@ const SearchDialog = ({ searchInfo, onClose }: Props) => {
                 </CheckboxGroup>
               </FormControl>
 
-              <FormControl>
-                <FormLabel>전공</FormLabel>
-                <CheckboxGroup
-                  colorScheme="green"
-                  value={searchOptions.majors}
-                  onChange={(values) => changeSearchOption('majors', values as string[])}
-                >
-                  <Wrap spacing={1} mb={2}>
-                    {searchOptions.majors.map(major => (
-                      <Tag key={major} size="sm" variant="outline" colorScheme="blue">
-                        <TagLabel>{major.split("<p>").pop()}</TagLabel>
-                        <TagCloseButton
-                          onClick={() => changeSearchOption('majors', searchOptions.majors.filter(v => v !== major))}/>
-                      </Tag>
-                    ))}
-                  </Wrap>
-                  <Stack spacing={2} overflowY="auto" h="100px" border="1px solid" borderColor="gray.200"
-                         borderRadius={5} p={2}>
-                    {allMajors.map(major => (
-                      <Box key={major}>
-                        <Checkbox key={major} size="sm" value={major}>
-                          {major.replace(/<p>/gi, ' ')}
-                        </Checkbox>
-                      </Box>
-                    ))}
-                  </Stack>
-                </CheckboxGroup>
-              </FormControl>
+              <SearchClass
+                allMajors={allMajors}
+                selectedMajors={searchOptions.majors}
+                onChangeMajors={(majors) => changeSearchOption('majors', majors)}
+              />
             </HStack>
             <Text align="right">
               검색결과: {filteredLectures.length}개
---
+++ src/searchClass.tsx
@@ -0,0 +1,75 @@
+import { useMemo, memo } from "react";
+import {
+  Box,
+  Checkbox,
+  CheckboxGroup,
+  FormControl,
+  FormLabel,
+  Stack,
+  Tag,
+  TagCloseButton,
+  TagLabel,
+  Wrap,
+} from "@chakra-ui/react";
+
+
+interface SearchClassProps {
+  allMajors: string[];
+  selectedMajors: string[];
+  onChangeMajors: (majors: string[]) => void;
+}
+
+const SearchClass = memo<SearchClassProps>(
+  ({ allMajors, selectedMajors, onChangeMajors }) => {
+    const selectedTags = useMemo(() => {
+      return selectedMajors.map((major) => (
+        <Tag key={major} size="sm" variant="outline" colorScheme="blue">
+          <TagLabel>{major.replace(/<p>/gi, " ")}</TagLabel>
+          <TagCloseButton
+            onClick={() =>
+              onChangeMajors(selectedMajors.filter((v) => v !== major))
+            }
+          />
+        </Tag>
+      ));
+    }, [selectedMajors, onChangeMajors]);
+
+    const checkboxList = useMemo(() => {
+      return allMajors.map((major) => (
+        <Box key={major}>
+          <Checkbox key={major} size="sm" value={major}>
+            {major.replace(/<p>/gi, " ")}
+          </Checkbox>
+        </Box>
+      ));
+    }, [allMajors]);
+
+    return (
+      <FormControl>
+        <FormLabel>전공</FormLabel>
+        <CheckboxGroup
+          colorScheme="green"
+          value={selectedMajors}
+          onChange={(vals) => onChangeMajors(vals as string[])}
+        >
+          <Wrap spacing={1} mb={2}>
+            {selectedTags}
+          </Wrap>
+          <Stack
+            spacing={2}
+            overflowY="auto"
+            h="100px"
+            border="1px solid"
+            borderColor="gray.200"
+            borderRadius={5}
+            p={2}
+          >
+            {checkboxList}
+          </Stack>
+        </CheckboxGroup>
+      </FormControl>
+    );
+  }
+);
+
+export default SearchClass;
