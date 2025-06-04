1878b8454cecf0427bb12d214dddd35336f33111
hanbeulYou <hanbeul.you@gmail.com>
2025-06-05 01:32:41 +0900

    refactor: dnd 관련 함수 및 훅 분리, useCallback으로 메모이제이션 추가

---
+++ src/createSnapModifier.ts
@@ -0,0 +1,36 @@
+import { Modifier } from "@dnd-kit/core";
+import { CellSize } from "./constants";
+
+export const createSnapModifier = (): Modifier => {
+  return ({ transform, containerNodeRect, draggingNodeRect }) => {
+    const containerTop = containerNodeRect?.top ?? 0;
+    const containerLeft = containerNodeRect?.left ?? 0;
+    const containerBottom = containerNodeRect?.bottom ?? 0;
+    const containerRight = containerNodeRect?.right ?? 0;
+
+    const { top = 0, left = 0, bottom = 0, right = 0 } = draggingNodeRect ?? {};
+
+    const minX = containerLeft - left + 120 + 1;
+    const minY = containerTop - top + 40 + 1;
+    const maxX = containerRight - right;
+    const maxY = containerBottom - bottom;
+
+    return {
+      ...transform,
+      x: Math.min(
+        Math.max(
+          Math.round(transform.x / CellSize.WIDTH) * CellSize.WIDTH,
+          minX
+        ),
+        maxX
+      ),
+      y: Math.min(
+        Math.max(
+          Math.round(transform.y / CellSize.HEIGHT) * CellSize.HEIGHT,
+          minY
+        ),
+        maxY
+      ),
+    };
+  };
+};
