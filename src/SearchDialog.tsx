89da8fcf37df19253d307d06a5ea467f38900715
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-06-06 01:09:02 +0900

    Fix: promise.all 방식 수정, cache 적용

--- src/SearchDialog.tsx
+++ src/SearchDialog.tsx
@@ -86,16 +86,35 @@ const fetchMajors = () => axios.get<Lecture[]>("/schedules-majors.json");
 const fetchLiberalArts = () =>
   axios.get<Lecture[]>("/schedules-liberal-arts.json");
 
-// TODO: 이 코드를 개선해서 API 호출을 최소화 해보세요 + Promise.all이 현재 잘못 사용되고 있습니다. 같이 개선해주세요.
-const fetchAllLectures = async () =>
-  await Promise.all([
-    (console.log("API Call 1", performance.now()), await fetchMajors()),
-    (console.log("API Call 2", performance.now()), await fetchLiberalArts()),
-    (console.log("API Call 3", performance.now()), await fetchMajors()),
-    (console.log("API Call 4", performance.now()), await fetchLiberalArts()),
-    (console.log("API Call 5", performance.now()), await fetchMajors()),
-    (console.log("API Call 6", performance.now()), await fetchLiberalArts()),
+const createCachedFetcher = () => {
+  const cache = new Map();
+
+  const cachedFetch = async (id: number, key: string, fetch: () => void) => {
+    console.log(`API Call ${id}`, performance.now());
+
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+
+    const result = fetch();
+    cache.set(key, result);
+
+    return result;
+  };
+
+  return cachedFetch;
+};
+
+const cachedFatch = createCachedFetcher();
+
+const fetchAllLectures = async () => {
+  const result = await Promise.all([
+    cachedFatch(1, "majors", fetchMajors),
+    cachedFatch(2, "liberalArts", fetchLiberalArts),
   ]);
+
+  return result;
+};
 
 // TODO: 이 컴포넌트에서 불필요한 연산이 발생하지 않도록 다양한 방식으로 시도해주세요.
 const SearchDialog = ({ searchInfo, onClose }: Props) => {
