c3bb0c456e9b9ed4ac5710e38e1b2741851bd0fd
서주희 <maywngml@naver.com>
2025-06-05 21:22:55 +0900

    ScheduleTabled의 그리드 영역 메모이제이션된 컴포넌트로 분리

--- src/components/ScheduleTable/ScheduleTableBody.tsx
+++ src/components/ScheduleTable/ScheduleTableBody.tsx
@@ -2,9 +2,6 @@ import { memo } from "react";
 import {
   Box,
   Button,
-  Flex,
-  Grid,
-  GridItem,
   Popover,
   PopoverArrow,
   PopoverBody,
@@ -13,12 +10,12 @@ import {
   PopoverTrigger,
   Text,
 } from "@chakra-ui/react";
-import { CellSize, DAY_LABELS, 분 } from "@/constants.ts";
+import ScheduleTableGrid from "./ScheduleTableGrid";
+import { CellSize, DAY_LABELS } from "@/constants.ts";
 import { Schedule } from "@/types.ts";
-import { fill2, parseHnM } from "@/lib/utils.ts";
 import { useDraggable } from "@dnd-kit/core";
 import { CSS } from "@dnd-kit/utilities";
-import { ComponentProps, Fragment } from "react";
+import { ComponentProps } from "react";
 
 interface ScheduleTableBodyProps {
   tableId: string;
@@ -33,18 +30,6 @@ interface ScheduleTableBodyProps {
     timeInfo: { day: string; time: number },
   ) => void;
 }
-
-const TIMES = [
-  ...Array(18)
-    .fill(0)
-    .map((v, k) => v + k * 30 * 분)
-    .map((v) => `${parseHnM(v)}~${parseHnM(v + 30 * 분)}`),
-
-  ...Array(6)
-    .fill(18 * 30 * 분)
-    .map((v, k) => v + k * 55 * 분)
-    .map((v) => `${parseHnM(v)}~${parseHnM(v + 50 * 분)}`),
-] as const;
 
 const ScheduleTableBody = ({
   tableId,
@@ -64,72 +49,10 @@ const ScheduleTableBody = ({
       position="relative"
       outline={isActive ? "5px dashed" : undefined}
       outlineColor="blue.300">
-      <Grid
-        templateColumns={`120px repeat(${DAY_LABELS.length}, ${CellSize.WIDTH}px)`}
-        templateRows={`40px repeat(${TIMES.length}, ${CellSize.HEIGHT}px)`}
-        bg="white"
-        fontSize="sm"
-        textAlign="center"
-        outline="1px solid"
-        outlineColor="gray.300">
-        <GridItem
-          key="교시"
-          borderColor="gray.300"
-          bg="gray.100">
-          <Flex
-            justifyContent="center"
-            alignItems="center"
-            h="full"
-            w="full">
-            <Text fontWeight="bold">교시</Text>
-          </Flex>
-        </GridItem>
-        {DAY_LABELS.map((day) => (
-          <GridItem
-            key={day}
-            borderLeft="1px"
-            borderColor="gray.300"
-            bg="gray.100">
-            <Flex
-              justifyContent="center"
-              alignItems="center"
-              h="full">
-              <Text fontWeight="bold">{day}</Text>
-            </Flex>
-          </GridItem>
-        ))}
-        {TIMES.map((time, timeIndex) => (
-          <Fragment key={`시간-${timeIndex + 1}`}>
-            <GridItem
-              borderTop="1px solid"
-              borderColor="gray.300"
-              bg={timeIndex > 17 ? "gray.200" : "gray.100"}>
-              <Flex
-                justifyContent="center"
-                alignItems="center"
-                h="full">
-                <Text fontSize="xs">
-                  {fill2(timeIndex + 1)} ({time})
-                </Text>
-              </Flex>
-            </GridItem>
-            {DAY_LABELS.map((day) => (
-              <GridItem
-                key={`${day}-${timeIndex + 2}`}
-                borderWidth="1px 0 0 1px"
-                borderColor="gray.300"
-                bg={timeIndex > 17 ? "gray.100" : "white"}
-                cursor="pointer"
-                _hover={{ bg: "yellow.100" }}
-                onClick={() =>
-                  onScheduleTimeClick?.(tableId, { day, time: timeIndex + 1 })
-                }
-              />
-            ))}
-          </Fragment>
-        ))}
-      </Grid>
-
+      <ScheduleTableGrid
+        tableId={tableId}
+        onScheduleTimeClick={onScheduleTimeClick}
+      />
       {schedules.map((schedule, index) => (
         <DraggableSchedule
           key={`${schedule.lecture.title}-${index}`}
