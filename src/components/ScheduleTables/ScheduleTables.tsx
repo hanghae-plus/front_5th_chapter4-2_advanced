29679fb4458cfb319d5deea21db85aa1a61f97dc
서주희 <maywngml@naver.com>
2025-06-05 18:38:58 +0900

    ScheduleTableHeader와 Body를 ScheduleTable 컴포넌트로 통합

--- src/components/ScheduleTables/ScheduleTables.tsx
+++ src/components/ScheduleTables/ScheduleTables.tsx
@@ -1,21 +1,17 @@
-import { Flex, Stack } from "@chakra-ui/react";
-import { ScheduleTableBody, ScheduleTableHeader } from "../ScheduleTable";
+import { Flex } from "@chakra-ui/react";
+import { ScheduleTable } from "../ScheduleTable";
 import { useScheduleContext } from "@/providers/ScheduleContext";
 import { SearchDialog } from "@/components/SearchDialog";
-import { useCallback, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { useDndContext } from "@dnd-kit/core";
 
 const ScheduleTables = () => {
-  const { schedulesMap, setSchedulesMap } = useScheduleContext();
+  const { schedulesMap } = useScheduleContext();
   const [searchInfo, setSearchInfo] = useState<{
     tableId: string;
     day?: string;
     time?: number;
   } | null>(null);
-
-  const disabledRemoveButton = useMemo(() => {
-    return Object.keys(schedulesMap).length === 1;
-  }, [schedulesMap]);
   const { active } = useDndContext();
 
   const activeTableId = useMemo(() => {
@@ -26,50 +22,9 @@ const ScheduleTables = () => {
     return null;
   }, [active]);
 
-  const handleAddScheduleClick = useCallback((tableId: string) => {
-    setSearchInfo((prev) => ({ ...prev, tableId }));
-  }, []);
-
-  const handleDuplicateScheduleClick = useCallback(
-    (targetId: string) => {
-      setSchedulesMap((prev) => ({
-        ...prev,
-        [`schedule-${Date.now()}`]: [...prev[targetId]],
-      }));
-    },
-    [setSchedulesMap],
-  );
-
-  const handleRemoveScheduleClick = useCallback(
-    (targetId: string) => {
-      setSchedulesMap((prev) => {
-        delete prev[targetId];
-        return { ...prev };
-      });
-    },
-    [setSchedulesMap],
-  );
-
-  const handleScheduleTimeClick = useCallback(
-    (tableId: string, timeInfo: { day: string; time: number }) => {
-      setSearchInfo({ tableId, ...timeInfo });
-    },
-    [],
-  );
-
-  const handleDeleteButtonClick = useCallback(
-    (tableId: string, timeInfo: { day: string; time: number }) => {
-      setSchedulesMap((prev) => ({
-        ...prev,
-        [tableId]: prev[tableId].filter(
-          (schedule) =>
-            schedule.day !== timeInfo.day ||
-            !schedule.range.includes(timeInfo.time),
-        ),
-      }));
-    },
-    [setSchedulesMap],
-  );
+  const disabledRemoveButton = useMemo(() => {
+    return Object.keys(schedulesMap).length === 1;
+  }, [schedulesMap]);
 
   return (
     <>
@@ -79,26 +34,14 @@ const ScheduleTables = () => {
         p={6}
         flexWrap="wrap">
         {Object.entries(schedulesMap).map(([tableId, schedules], index) => (
-          <Stack
-            key={tableId}
-            width="600px">
-            <ScheduleTableHeader
-              index={index}
-              tableId={tableId}
-              disabledRemoveButton={disabledRemoveButton}
-              onAddScheduleClick={handleAddScheduleClick}
-              onDuplicateScheduleClick={handleDuplicateScheduleClick}
-              onRemoveScheduleClick={handleRemoveScheduleClick}
-            />
-            <ScheduleTableBody
-              key={`schedule-table-${index}`}
-              schedules={schedules}
-              tableId={tableId}
-              isActive={activeTableId === tableId}
-              onScheduleTimeClick={handleScheduleTimeClick}
-              onDeleteButtonClick={handleDeleteButtonClick}
-            />
-          </Stack>
+          <ScheduleTable
+            tableId={tableId}
+            schedules={schedules}
+            index={index}
+            isActive={activeTableId === tableId}
+            disabledRemoveButton={disabledRemoveButton}
+            setSearchInfo={setSearchInfo}
+          />
         ))}
       </Flex>
       <SearchDialog
